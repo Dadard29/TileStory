ec471c3502a7e22722fdf0e1f6d908a8c6e4f71f
florian.charpentier <florian.charpentier@epita.fr>
2021-11-30 21:08:32 +0100

    preparing for release

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,28 +1,19 @@
 extends CanvasLayer
 
 onready var full_rect = $FullRect
-onready var level_box = $FullRect/CenterContainer/VB/LevelSelection/LevelPanel/LevelVBox/LevelBox
-onready var score_value = $FullRect/CenterContainer/VB/Score/ScorePanel/ScoreBox/ScoreValue
+onready var level_box = $FullRect/Panel/Center/VB/LevelSelection/LevelPanel/LevelVBox/LevelBox
+onready var score_value = $FullRect/Panel/Center/VB/Score/ScorePanel/ScoreBox/ScoreValue
 
 signal level(level_path)
 
 const LEVEL_BASE = "res://scenes/levels/"
 
+
 var level_selector = preload("res://scenes/LevelSelector.tscn")
-var levels_data: Array = [
-	{
-		"icon": preload("res://images/icons/player_icon.png"),
-		"name": "Awake"
-	},
-	{
-		"icon": preload("res://images/icons/energy_small_icon.png"),
-		"name": "Power up"
-	},
-	{
-		"icon": preload("res://images/icons/spike_icon.png"),
-		"name": "Beware"
-	}
+var levels_names: Array = [
+	"Awake", "Power Up", "Beware", "Optional", "Bounce it", "Slow motion"
 ]
+var level_default_icon = preload("res://images/icons/player_icon.png")
 
 func _list_levels():
 	var files: Array = []
@@ -52,10 +43,14 @@ func _ready() -> void:
 		var new_level_selector = level_selector.instance()
 		
 		var level_path = LEVEL_BASE + level_file
-		var level_name = levels_data[i]["name"]
-		var icon = levels_data[i]["icon"]
 		
-		new_level_selector.set_level_path(level_path, level_name, icon)
+		var level_name: String
+		if i >= len(levels_names):
+			level_name = "Level " + String(i)
+		else:
+			level_name = levels_names[i]
+		
+		new_level_selector.set_level_path(level_path, level_name)
 		level_box.add_child(new_level_selector)
 		
 		new_level_selector.connect("level_selected", self, "_emit_level_signal")
@@ -69,7 +64,7 @@ func show():
 	full_rect.set_visible(true)
 
 func set_score(new_score: int):
-	var score = String(new_score) + " pts"
+	var score = String(new_score) + "pts"
 	score_value.set_text(score)
 
 func _on_QuitButton_pressed() -> void:
--- scripts/nodes/LevelSelector.gd
+++ scripts/nodes/LevelSelector.gd
@@ -4,18 +4,15 @@ onready var button = $Button
 
 var _level_path: String = ""
 var _level_name: String = ""
-var _icon: Texture
 
 signal level_selected(level_path)
 
 func _ready() -> void:
 	button.set_text(_level_name)
-	button.set_button_icon(_icon)
 	
-func set_level_path(level_path: String, level_name: String, icon: Texture):
+func set_level_path(level_path: String, level_name: String):
 	_level_path = level_path
 	_level_name = level_name
-	_icon = icon
 
 func _on_Button_pressed() -> void:
 	emit_signal("level_selected", _level_path)
